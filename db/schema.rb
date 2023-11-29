55c8baed9832903a435057b587d78d8ab429e32e
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-11-29 13:06:11 -0400

    Add validation to users

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_212937) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
