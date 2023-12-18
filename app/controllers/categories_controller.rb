bd4c3eca7247c3f1a9b34cdc2a735f6a5a20bc3c
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-12-18 09:21:27 -0400

    update navbar with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,10 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[show edit update destroy]
+  before_action :require_admin, except: %i[index show]
   # action``
-  def index; end
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
 
   # action
   def show; end
@@ -41,5 +44,13 @@ class CategoriesController < ApplicationController
   # category params
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  # require_user
+  def require_admin
+    unless logged_in? && current_user.admin?
+      flash[:alert] = 'Only Admins can performs that action'
+      redirect_to(categories_path)
+    end
   end
 end
--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 describe 'Create category', :integration do
+  def setup
+    @admin = User.create!(username: 'Marce', email: 'daddas@dasd.com', password: '1234', admin: true, faction: 'Cobra')
+  end
+
   it 'get new category form and create a new category' do
+    sign_in_as(@admin)
     get '/categories/new'
     assert_response :success
     assert_difference 'Category.count', 1 do
@@ -14,12 +19,13 @@ describe 'Create category', :integration do
   end
 
   it 'get new category form and reject invalid category submission' do
+    sign_in_as(@admin)
     get '/categories/new'
     assert_response :success
     assert_no_difference 'Category.count' do
       post categories_path, params: { category: { name: '' } }
     end
-    assert_select 'div#toast-dangers'
+    assert_select 'div#toast-danger'
     assert_match 'errors', response.body
   end
 end
